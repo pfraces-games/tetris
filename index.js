2523b02a6825f4105332016b3c4b6ff65d3fc9aa
Pau Fracés <pfraces@gmail.com>
2013-02-14 23:23:45 +0100

    refactor with components

--- index.js
+++ index.js
@@ -1,66 +1,32 @@
 var gen = require('gen');
 
-module.exports = function (config) {
+module.exports = tetris;
+
+var tetris = function (config) {
   return new Tetris(config);
 };
 
-var private = {
-  cursor: { x: 0, y: 0 }
-};
-
-var engine;
-/* var player; */
-
 var Tetris = function (config) {
-  engine = gen(config);
-};
-
-Tetris.prototype.start = function () {
-  /* player = engine.create.entity(); */
-  /* engine.start() debe dibujar entidades */
-  engine.start();
-  engine.map.at(private.cursor).color = 'blue';
-  engine.draw.cell(private.cursor);
+  this.engine = gen(config);
+  this.player = engine.actor();
 };
 
 Tetris.prototype.keymap = function (keymap) {
-  engine.keymap = keymap;
+  this.engine.keymap(keymap);
 };
 
 Tetris.prototype.up = function () {
-  /* player.move.up(); // teleport
-     player.go.up(); // path finding */
-  if (private.cursor.y === 0) return;
-  engine.map.at(private.cursor).color = engine.board.cell.color;
-  engine.draw.cell(private.cursor);
-  private.cursor.y--;
-  engine.map.at(private.cursor).color = 'blue';
-  engine.draw.cell(private.cursor);
+  this.player.move.up();
 };
 
 Tetris.prototype.down = function () {
-  if (private.cursor.y === (engine.board.height - 1)) return;
-  engine.map.at(private.cursor).color = engine.board.cell.color;
-  engine.draw.cell(private.cursor);
-  private.cursor.y++;
-  engine.map.at(private.cursor).color = 'blue';
-  engine.draw.cell(private.cursor);
+  this.player.move.down();
 };
 
 Tetris.prototype.left = function () {
-  if (private.cursor.x === 0) return;
-  engine.map.at(private.cursor).color = engine.board.cell.color;
-  engine.draw.cell(private.cursor);
-  private.cursor.x--;
-  engine.map.at(private.cursor).color = 'blue';
-  engine.draw.cell(private.cursor);
+  this.player.move.left();
 };
 
 Tetris.prototype.right = function () {
-  if (private.cursor.x === (engine.board.width - 1)) return;
-  engine.map.at(private.cursor).color = engine.board.cell.color;
-  engine.draw.cell(private.cursor);
-  private.cursor.x++;
-  engine.map.at(private.cursor).color = 'blue';
-  engine.draw.cell(private.cursor);
+  this.player.move.right();
 };
