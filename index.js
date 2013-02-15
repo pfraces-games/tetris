64f755a35c9b5044adb11bd7c6a45cd8b28d94c8
Pau Fracés <pfraces@gmail.com>
2013-02-15 19:54:37 +0100

    [bugfix] component pattern

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 var gen = require('gen');
 
-var tetris = module.exports = function (config) {
+module.exports = function (config) {
   return new Tetris(config);
 };
 
 var Tetris = function (config) {
+  var self = this;
   this.engine = gen(config);
   this.player = this.engine.actor({ x: 0, y: 0 });
+
+  this.up = function () {
+    self.player.move.up();
+  };
+
+  this.down = function () {
+    self.player.move.down();
+  };
+
+  this.left = function () {
+    self.player.move.left();
+  };
+
+  this.right = function () {
+    self.player.move.right();
+  };
 };
 
 Tetris.prototype.keymap = function (keymap) {
-  tetris.engine.keymap(keymap);
-};
-
-Tetris.prototype.up = function () {
-  tetris.player.move.up();
-};
-
-Tetris.prototype.down = function () {
-  tetris.player.move.down();
-};
-
-Tetris.prototype.left = function () {
-  tetris.player.move.left();
-};
-
-Tetris.prototype.right = function () {
-  tetris.player.move.right();
+  this.engine.keymap(keymap);
 };
