97020be6fe5c68c56fa2e33f71eb1e51f6512188
Pau Fracés <pfraces@gmail.com>
2013-02-18 02:46:48 +0100

    [bugfix] disabling movement on keydown

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var gen = require('gen');
+var gen = require('gen'),
+    shortcut = require('shortcut');
 
 module.exports = function (config) {
   return new Tetris(config);
@@ -10,23 +11,46 @@ var Tetris = function (config) {
 
   var player = this.player;
 
-  this.up = function () {
-    player.actions.set('up', player.move.up);
+  this.up = function (enable) {
+    if (enable) {
+      player.actions.set('up', player.move.up);
+    } else {
+      player.actions.del('up');
+    }
   };
 
-  this.down = function () {
-    player.actions.set('down', player.move.down);
+  this.down = function (enable) {
+    if (enable) {
+      player.actions.set('down', player.move.down);
+    } else {
+      player.actions.del('down');
+    }
   };
 
-  this.left = function () {
-    player.actions.set('left', player.move.left);
+  this.left = function (enable) {
+    if (enable) {
+      player.actions.set('left', player.move.left);
+    } else {
+      player.actions.del('left');
+    }
   };
 
-  this.right = function () {
-    player.actions.set('right', player.move.right);
+  this.right = function (enable) {
+    if (enable) {
+      player.actions.set('right', player.move.right);
+    } else {
+      player.actions.del('right');
+    }
   };
 };
 
 Tetris.prototype.keymap = function (keymap) {
-  this.engine.keymap(keymap);
+  Object.keys(keymap).forEach(function (key) {
+    shortcut.on(key, function () {
+      keymap[key](true);
+    });
+    shortcut.onEnd(key, function () {
+      keymap[key]();
+    });
+  });
 };
